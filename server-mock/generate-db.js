a403d5888373e99f5369e129be72892b0ccea489
Adrian Antonescu <antonescu.adrian16@gmail.com>
2025-11-27 08:16:10 +0200

    Added script to generate bookmarks

---
+++ server-mock/generate-db.js
@@ -0,0 +1,66 @@
+const fs = require("fs");
+const path = require("path");
+
+const dbFilePath = path.join(__dirname, "db.json");
+
+const getDateOffset = (daysAgo) => {
+  const date = new Date();
+  date.setDate(date.getDate() - daysAgo);
+  return date.toISOString();
+};
+
+const bookmarks = [
+  {
+    id: crypto.randomUUID(),
+    updatedAt: getDateOffset(0),
+    name: "Angular",
+    url: "https://angular.io",
+  },
+  {
+    id: crypto.randomUUID(),
+    updatedAt: getDateOffset(1),
+    name: "json-server",
+    url: "http://localhost:3000/bookmarks",
+  },
+  {
+    id: crypto.randomUUID(),
+    updatedAt: getDateOffset(1),
+    name: "bookmarks-app",
+    url: "http://localhost:4200",
+  },
+  {
+    id: crypto.randomUUID(),
+    updatedAt: getDateOffset(3),
+    name: "GitHub",
+    url: "https://github.com",
+  },
+  {
+    id: crypto.randomUUID(),
+    updatedAt: getDateOffset(4),
+    name: "Medium",
+    url: "https://medium.com",
+  },
+  {
+    id: crypto.randomUUID(),
+    updatedAt: getDateOffset(5),
+    name: "Google",
+    url: "https://google.com",
+  },
+  {
+    id: crypto.randomUUID(),
+    updatedAt: getDateOffset(6),
+    name: "TypeScript",
+    url: "https://www.typescriptlang.org",
+  },
+  {
+    id: crypto.randomUUID(),
+    updatedAt: getDateOffset(7),
+    name: "Dev.to",
+    url: "https://dev.to",
+  },
+];
+
+const db = { bookmarks };
+
+fs.writeFileSync(dbFilePath, JSON.stringify(db, null, 2));
+console.log("db.json generated successfully with manually defined bookmarks!");
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { BookmarksModule } from './features/bookmarks/bookmarks.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
 import { bookmarkReducer } from './features/bookmarks/store/bookmarks.reducer';
 import { AppState } from './app.state';
 
@@ -19,6 +20,7 @@ import { AppState } from './app.state';
     BookmarksModule,
     BrowserAnimationsModule,
     StoreModule.forRoot<AppState>({ bookmark: bookmarkReducer }),
+    StoreDevtoolsModule.instrument(),
   ],
   providers: [],
   bootstrap: [AppComponent],
